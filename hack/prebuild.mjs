d484664e3bb16a7b8286a4cb5cead4c19c78986a
Alberto Marchetti <cmaster11@gmail.com>
2024-05-30 06:32:43 +0200

    WIP

--- hack/prebuild.mjs
+++
@@ -1,8 +0,0 @@
-const __dirname = import.meta.dirname;
-const rootDir = path.join(__dirname, '..');
-
-const deps = require(path.join(rootDir, 'package.json')).devDependencies;
-const actionToBuild = minimist(process.argv.slice(3))._[0];
-
-await $`npm install esbuild@${deps.esbuild}`;
-await $`npx tsx ${path.join(__dirname, 'build.mts')} ${actionToBuild}`.quiet();
