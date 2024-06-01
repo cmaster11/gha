d6a6b233a574ca041ece35345b749325fd9fe538
Alberto Marchetti <cmaster11@gmail.com>
2024-06-01 06:20:29 +0200

    WIP

--- lib/copy-action-files.ts
+++ lib/copy-action-files.ts
@@ -1,5 +1,7 @@
 import path from 'node:path';
 import { actionsDir } from './constants.js';
+import { parse, stringify } from 'yaml';
+import { string } from 'joi';
 
 export async function copyActionFiles(actionName: string) {
   const tmpDir = tmpdir();
@@ -8,10 +10,27 @@ export async function copyActionFiles(actionName: string) {
   const actionDir = path.join(actionsDir, actionName);
 
   // Copy over all relevant action files
-  await fs.copy(
-    path.join(actionDir, 'action.yml'),
-    path.join(tmpDir, 'action.yml')
-  );
+
+  // Fix the action.yml file to strip out any PREBUILD steps
+  {
+    const actionYmlContents = parse(
+      await fs.readFile(path.join(actionDir, 'action.yml'), 'utf-8')
+    );
+
+    if ('runs' in actionYmlContents && 'steps' in actionYmlContents.runs) {
+      const idx = (actionYmlContents.runs.steps as { id: string }[]).findIndex(
+        (step) => step.id == 'PREBUILD'
+      );
+      if (idx > 0) {
+        actionYmlContents.runs.steps.splice(idx, 1);
+      }
+    }
+
+    await fs.writeFile(
+      path.join(tmpDir, 'action.yml'),
+      stringify(actionYmlContents)
+    );
+  }
 
   if (await fs.exists(path.join(actionDir, 'dist')))
     await fs.copy(path.join(actionDir, 'dist'), path.join(tmpDir, 'dist'));
