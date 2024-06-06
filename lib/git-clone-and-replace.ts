07072b371d288b03badaad25142ef11ffb4c7b5f
Alberto Marchetti <cmaster11@gmail.com>
2024-06-06 08:37:54 +0200

    action-ci-build

--- lib/git-clone-and-replace.ts
+++ lib/git-clone-and-replace.ts
@@ -17,10 +17,11 @@ export async function flowGitCloneReplaceAndCommit(
     `[flowGitCloneReplaceAndCommit] Executing in directory ${tmpDir}`
   );
 
+  // Clone the local git repo dr
+  await fs.copy(path.join(rootDir, '.git'), path.join(tmpDir, '.git'));
+
   await within(async () => {
     cd(tmpDir);
-
-    await fs.copy(path.join(rootDir, '.git'), path.join(tmpDir, '.git'));
 
     // If there is already a version branch, restore it, so we can
     // have a nice commit history
@@ -31,7 +32,7 @@ export async function flowGitCloneReplaceAndCommit(
     await fs.copy(contentsDir, tmpDir);
 
     // Make sure to always create a new commit
-    await fs.writeFile(tmpDir + '/.timestamp', new Date().getTime().toString());
+    await fs.writeFile('.timestamp', new Date().getTime().toString());
 
     await $`git add .`;
 
